de24b65fc648f75bf7e0dde30d0818481164da06
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-06-03 16:33:44 -0500

    added startgame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-//window.alert("Welcome to the JavaScript Quiz App! \n \nYour final score is determined by the number of questions answered correctly within the *** second time limit. A skipped question will incur a penalty of *** seconds. Click START to begin.")
+window.alert("Welcome to the JavaScript Quiz App! \n \nYour final score is determined by the number of questions answered correctly within the time limit. A skipped question will incur a penalty that reduces your remaining time.  Click START to begin.")
 var skipButton = document.querySelector('#skip-btn')
+var startButton = document.querySelector('#start-btn')
 var questionContainer = document.querySelector('#question-container')
 var questionEl = document.querySelector("#question")
 var answerButton = document.querySelector("#answer-buttons")
@@ -18,12 +19,11 @@ function loadQuestion(question) {
     // keeps track of which item in the answers array we are iterating through
     var arrayCount = 0
 
-    //creates a button for each answer in the answers array with class of btn and answer-btn
+    //creates a button for each answer in the answers array with class of btn
     question.answers.forEach(function(answer){
         var button = document.createElement("button")
         button.innerText = answer.text
-        button.classList.add('btn')
-        button.classList.add('answer-btn')
+        button.classList.add('btn')       
     // copy data attribute from answers array to each button 
        button.dataset.correct = (question.answers[arrayCount].correct)
        
@@ -60,9 +60,18 @@ var checkAnswer = function() {
         console.log("wrong answer")
     }
 }
+var startGame = function() {
+    startButton.classList.add("hide")
+    questionContainer.classList.remove("hide")
+    skipButton.classList.remove("hide")
+
+}
+
+
+// Event listeners
 answerButton.addEventListener('click', checkAnswer)
 skipButton.addEventListener('click', skipQuestion)
-
+startButton.addEventListener('click', startGame)
 
 
 
@@ -97,8 +106,17 @@ var questions = [
             {text: "q3 answer 4 text", correct: false}
         
         ]
+    },
+    {
+        question: "This is the fourth question",
+        answers: [
+            {text: "q3 answer 1 text", correct: false},
+            {text: "q3 answer 2 text", correct: false},
+            {text: "q3 answer 3 text", correct: false},
+            {text: "q3 answer 4 text", correct: true}
+        
+        ]
     }
-
-    ]
+]
     
     loadQuestion(questions[questionCounter])
